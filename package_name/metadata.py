18cccec980a95976063f8b93a0cfea8457b8ad86
Eric Lopes <eric.oliveira.lopes@gmail.com>
2016-10-23 11:15:58 +0000

    testing

---
+++ package_name/metadata.py
@@ -0,0 +1,14 @@
+"""package_name metadata
+"""
+
+package = "package_name"
+project = "proj-name"
+version = "1.0"
+keywords = "key1 key2"
+release = "Alpha"
+author = "Me and You"
+email = "foo@bar.com"
+year = "2016"
+url = "http://foobar.com/"
+license = "agpl"
+short_description = "A short description for the project"
